09d97b571f4341ba22159c244422065b28f0291a
Hyunggeun447 <bopi447@naver.com>
2022-10-24 19:29:17 +0900

    test: changeNickname

--- src/test/java/solo_project/solo_project/domain/user/entity/UserTest.java
+++ src/test/java/solo_project/solo_project/domain/user/entity/UserTest.java
@@ -3,6 +3,8 @@ package solo_project.solo_project.domain.user.entity;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,17 +13,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserTest {
 
+  String email = "admin@naver.com";
+  String firstName = "Chun";
+  String lastName = "Park";
+  String nickname = "yapyap";
+  String phoneNumber = "010-1234-5678";
+  String city = "Seoul";
+  String detailAddress = "1-1";
+
   @Nested
   @DisplayName("생성")
   class create {
-
-    String email = "admin@naver.com";
-    String firstName = "Chun";
-    String lastName = "Park";
-    String nickname = "yapyap";
-    String phoneNumber = "010-1234-5678";
-    String city = "Seoul";
-    String detailAddress = "1-1";
 
     @Test
     @DisplayName("성공")
@@ -50,6 +52,48 @@ class UserTest {
       //then
       assertThrows(RuntimeException.class,
           () -> new User(email, firstName, lastName, nickname, phoneNumber, city, detailAddress));
+    }
+  }
+
+
+  @Nested
+  @DisplayName("수정")
+  class update {
+
+    User user;
+
+    @BeforeEach
+    void setup() {
+      user = new User(email, firstName, lastName, nickname, phoneNumber, city, detailAddress);
+    }
+
+    @AfterEach
+    void deleteAll() {
+      user = null;
+    }
+
+    String newNickname = "paypay";
+
+    @Test
+    @DisplayName("성공")
+    public void S() throws Exception {
+
+      //when
+      user.changeNickname(newNickname);
+
+      //then
+      assertThat(user.getNickname()).isEqualTo(newNickname);
+    }
+
+    @Test
+    @DisplayName("실패 - newNickname is null")
+    public void F_newNickname() throws Exception {
+
+      //when
+      newNickname = null;
+
+      //then
+      assertThrows(RuntimeException.class, () -> user.changeNickname(newNickname));
     }
   }
 }
