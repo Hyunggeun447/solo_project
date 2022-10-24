d56d7c979f039d2be31cdbba1d113fd2e3ef24ef
Hyunggeun447 <bopi447@naver.com>
2022-10-24 19:29:26 +0900

    refactor:

--- src/test/java/solo_project/solo_project/domain/user/entity/ProfileTest.java
+++ src/test/java/solo_project/solo_project/domain/user/entity/ProfileTest.java
@@ -12,35 +12,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProfileTest {
 
+  /**
+   * User - email - firstName - lastName - nickname - phoneNumber - city - detailAddress
+   */
+  String email = "admin@naver.com";
+  String firstName = "Chun";
+  String lastName = "Park";
+  String nickname = "yapyap";
+  String phoneNumber = "010-1234-5678";
+  String city = "Seoul";
+  String detailAddress = "1-1";
+  User user = new User(email, firstName, lastName, nickname, phoneNumber, city, detailAddress);
+
+  /**
+   * Profile - profileUrl
+   */
+  String profileUrl = "/s3/.../profile1";
 
   @Nested
   @DisplayName("생성")
   class create {
-
-    /**
-     * User
-     * - email
-     * - firstName
-     * - lastName
-     * - nickname
-     * - phoneNumber
-     * - city
-     * - detailAddress
-     */
-    String email = "admin@naver.com";
-    String firstName = "Chun";
-    String lastName = "Park";
-    String nickname = "yapyap";
-    String phoneNumber = "010-1234-5678";
-    String city = "Seoul";
-    String detailAddress = "1-1";
-    User user = new User(email, firstName, lastName, nickname, phoneNumber, city, detailAddress);
-
-    /**
-     * Profile
-     * - profileUrl
-     */
-    String profileUrl = "/s3/.../profile1";
 
     @Test
     @DisplayName("성공")
