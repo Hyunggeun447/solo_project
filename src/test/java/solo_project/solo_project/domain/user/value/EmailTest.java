a0185b2c6fc3a0cc1134600882e89483021c6259
Hyunggeun447 <bopi447@naver.com>
2022-10-24 15:38:49 +0900

    test: EmailTest

---
+++ src/test/java/solo_project/solo_project/domain/user/value/EmailTest.java
@@ -0,0 +1,59 @@
+package solo_project.solo_project.domain.user.value;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailTest {
+
+  String emailString = "email@naver.com";
+
+  @Test
+  @DisplayName("생성 성공")
+  public void S() throws Exception {
+
+    //when
+    Email email = new Email(emailString);
+
+    //then
+    assertThat(email.getEmail()).isEqualTo(emailString);
+  }
+
+  @Test
+  @DisplayName("생성 실패 - emailString is null ")
+  public void F_emailString() throws Exception {
+
+    //given
+    String emailString = null;
+
+    //then
+    assertThrows(RuntimeException.class, () -> new Email(emailString));
+  }
+
+  @Test
+  @DisplayName("생성 실패 - emailString isn't match regex - not exist @")
+  public void F_emailString_regex() throws Exception {
+
+    //given
+    String emailString = "email";
+
+    //then
+    assertThrows(RuntimeException.class, () -> new Email(emailString));
+  }
+
+  @Test
+  @DisplayName("생성 실패 - emailString isn't match regex - not exist @ after string")
+  public void F_emailString_regex2() throws Exception {
+
+    //given
+    String emailString = "email@";
+
+    //then
+    assertThrows(RuntimeException.class, () -> new Email(emailString));
+  }
+}
