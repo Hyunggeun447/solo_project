724bc38fac5b5d8902f775fa313f0d9b9af0b67e
Hyunggeun447 <bopi447@naver.com>
2022-10-24 13:23:27 +0900

    feat: Name

---
+++ src/main/java/solo_project/solo_project/domain/user/entity/Name.java
@@ -0,0 +1,28 @@
+package solo_project.solo_project.domain.user.entity;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.util.Assert;
+
+@Embeddable
+@Getter
+@Access(value = AccessType.FIELD)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Name {
+
+  private String FirstName;
+  private String LastName;
+
+  public Name(String firstName, String lastName) {
+    Assert.notNull(firstName, "need firstName");
+    Assert.notNull(lastName, "need lastName");
+    FirstName = firstName;
+    LastName = lastName;
+  }
+}
