cbe927182d5e436cc4748a7597b94ab8d9e2fb58
Hyunggeun447 <bopi447@naver.com>
2022-10-24 15:31:34 +0900

    test: AddressTest

---
+++ src/test/java/solo_project/solo_project/domain/user/value/AddressTest.java
@@ -0,0 +1,48 @@
+package solo_project.solo_project.domain.user.value;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressTest {
+
+  String seoul = "seoul";
+  String detailAddress = "20-1";
+  @Test
+  @DisplayName("생성 성공")
+  public void S() throws Exception {
+
+    //when
+    Address address = new Address(seoul, detailAddress);
+
+    //then
+    Assertions.assertThat(address.getCity()).isEqualTo(seoul);
+    Assertions.assertThat(address.getDetailAddress()).isEqualTo(detailAddress);
+  }
+
+  @Test
+  @DisplayName("생성 실패 - city is null")
+  public void F() throws Exception {
+
+    //when
+    seoul = null;
+
+    //then
+    assertThrows(RuntimeException.class, () -> new Address(seoul, detailAddress));
+  }
+  @Test
+  @DisplayName("생성 실패 - detailAddress is null")
+  public void F_detailAddress() throws Exception {
+
+    //when
+    detailAddress = null;
+
+    //then
+    assertThrows(RuntimeException.class, () -> new Address(seoul, detailAddress));
+  }
+
+}
