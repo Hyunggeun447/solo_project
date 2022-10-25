c50f5482801d9e0d08e36d67bdad0826a5e30a0f
Hyunggeun447 <bopi447@naver.com>
2022-10-25 16:28:15 +0900

    feat: WebSecurityConfig

---
+++ src/main/java/solo_project/solo_project/domain/user/config/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package solo_project.solo_project.domain.user.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
