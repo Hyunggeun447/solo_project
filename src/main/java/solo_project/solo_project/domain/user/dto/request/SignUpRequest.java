2e6d471b7c35f4302fd03d48e12c6ad1f1bf4361
Hyunggeun447 <bopi447@naver.com>
2022-10-25 19:31:51 +0900

    feat: SignUpRequest and Converter

---
+++ src/main/java/solo_project/solo_project/domain/user/dto/request/SignUpRequest.java
@@ -0,0 +1,35 @@
+package solo_project.solo_project.domain.user.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class SignUpRequest {
+
+  private String email;
+
+  private String password;
+
+  private String firstName;
+
+  private String lastName;
+
+  private String nickname;
+
+  private String phoneNumber;
+
+  private String city;
+
+  private String detailAddress;
+
+  public SignUpRequest(String email, String password, String firstName, String lastName,
+      String nickname, String phoneNumber, String city, String detailAddress) {
+    this.email = email;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.nickname = nickname;
+    this.phoneNumber = phoneNumber;
+    this.city = city;
+    this.detailAddress = detailAddress;
+  }
+}
---
+++ src/main/java/solo_project/solo_project/domain/user/util/UserConverter.java
@@ -0,0 +1,21 @@
+package solo_project.solo_project.domain.user.util;
+
+import solo_project.solo_project.domain.user.dto.request.SignUpRequest;
+import solo_project.solo_project.domain.user.entity.User;
+
+public class UserConverter {
+
+  public static User toUser(SignUpRequest request) {
+    return new User(
+        request.getEmail(),
+        request.getFirstName(),
+        request.getLastName(),
+        request.getNickname(),
+        request.getPhoneNumber(),
+        request.getCity(),
+        request.getDetailAddress()
+    );
+
+  }
+
+}
