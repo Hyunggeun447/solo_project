7d67db6db85e4c86da72c35633f896ec7d1d09a6
Hyunggeun447 <bopi447@naver.com>
2022-10-26 17:47:15 +0900

    feat: LoginResponse and Converter

---
+++ src/main/java/solo_project/solo_project/domain/user/dto/response/LoginResponse.java
@@ -0,0 +1,33 @@
+package solo_project.solo_project.domain.user.dto.response;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginResponse {
+
+  private String accessToken;
+
+  private LoginUser loginUser;
+
+
+  @Builder
+  @NoArgsConstructor(access = AccessLevel.PROTECTED)
+  @AllArgsConstructor(access = AccessLevel.PROTECTED)
+  public class LoginUser {
+
+    private Long id;
+
+    private String nickname;
+
+    private String profileImage;
+
+    private List<String> authorities;
+
+  }
+}
--- src/main/java/solo_project/solo_project/domain/user/util/UserConverter.java
+++ src/main/java/solo_project/solo_project/domain/user/util/UserConverter.java
@@ -1,6 +1,8 @@
 package solo_project.solo_project.domain.user.util;
 
 import solo_project.solo_project.domain.user.dto.request.SignUpRequest;
+import solo_project.solo_project.domain.user.dto.response.LoginResponse;
+import solo_project.solo_project.domain.user.dto.response.LoginResponse.LoginUser;
 import solo_project.solo_project.domain.user.entity.User;
 
 public class UserConverter {
@@ -16,7 +18,21 @@ public class UserConverter {
         request.getDetailAddress(),
         request.getPassword()
     );
+  }
 
+  public static LoginResponse toLoginResponse(User user, String token) {
+    LoginResponse response = LoginResponse.builder()
+        .accessToken(token)
+        .loginUser(
+            LoginUser.builder()
+                .id(user.getId())
+                .nickname(user.getNickname().getNickname())
+                .profileImage(user.getMainProfile().getProfileUrl())
+                .authorities(user.getAuthorities())
+                .build()
+        )
+        .build();
+    return response;
   }
 
 }
