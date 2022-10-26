c5e6e6b144be5433c80a40d01ed775f21de7f5f5
Hyunggeun447 <bopi447@naver.com>
2022-10-26 19:53:00 +0900

    feat: singUp, login

--- src/main/java/solo_project/solo_project/domain/user/controller/UserController.java
+++ src/main/java/solo_project/solo_project/domain/user/controller/UserController.java
@@ -1,15 +1,38 @@
 package solo_project.solo_project.domain.user.controller;
 
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import solo_project.solo_project.common.s3.UploadService;
+import solo_project.solo_project.domain.user.dto.request.LoginRequest;
 import solo_project.solo_project.domain.user.dto.request.SignUpRequest;
+import solo_project.solo_project.domain.user.dto.response.LoginResponse;
+import solo_project.solo_project.domain.user.service.UserService;
 
 @RestController
 @RequestMapping("/api/v1/users")
-public interface UserController {
+@RequiredArgsConstructor
+public class UserController {
 
-  ResponseEntity<Long> signUp(@RequestBody SignUpRequest request);
+  private final UserService userService;
+  private final UploadService uploadService;
 
+  @PostMapping("/signUp")
+  public ResponseEntity<Long> signUp(@RequestBody SignUpRequest request) {
+    Long userId = userService.signUp(request);
+    // TODO: 2022/10/26 URI 설정
+    return ResponseEntity.created(URI.create("/")).body(userId);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+    LoginResponse loginResponse = userService.login(request);
+
+    return ResponseEntity.ok(loginResponse);
+
+  }
 }
