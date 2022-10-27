8bbd77b47f2c5017d7cde12f198aaf7296b961a5
Hyunggeun447 <bopi447@naver.com>
2022-10-27 16:12:50 +0900

    feat: UpdatePasswordRequest

---
+++ src/main/java/solo_project/solo_project/domain/user/dto/request/UpdatePasswordRequest.java
@@ -0,0 +1,18 @@
+package solo_project.solo_project.domain.user.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UpdatePasswordRequest {
+
+  private String prePassword;
+
+  private String newPassword;
+}
