0920dd7cac0d017a4f076325080184870b5776c2
Hyunggeun447 <bopi447@naver.com>
2022-10-27 15:20:18 +0900

    feat: add Transactional readOnly, add update Method

--- src/main/java/solo_project/solo_project/domain/user/service/UserService.java
+++ src/main/java/solo_project/solo_project/domain/user/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import solo_project.solo_project.domain.user.dto.request.LoginRequest;
 import solo_project.solo_project.domain.user.dto.request.SignUpRequest;
+import solo_project.solo_project.domain.user.dto.request.UpdateRequest;
 import solo_project.solo_project.domain.user.dto.response.LoginResponse;
 import solo_project.solo_project.domain.user.entity.User;
 import solo_project.solo_project.domain.user.repository.UserRepository;
@@ -47,10 +48,21 @@ public class UserService {
     return toLoginResponse(user, token);
   }
 
+  public void update(Long userId, UpdateRequest request) {
+    User user = userRepository.findById(userId)
+        .orElseThrow(RuntimeException::new);
+
+    user.changeNickname(request.getNickname());
+    user.changeAddress(request.getCity(), request.getDetailAddress());
+    user.changePhoneNumber(request.getPhoneNumber());
+  }
+
+  @Transactional(readOnly = true)
   public boolean isDuplicatedNickname(String nickname) {
     return userRepository.existsByNicknameNickname(nickname);
   }
 
+  @Transactional(readOnly = true)
   public boolean isDuplicatedEmail(String email) {
     return userRepository.existsByEmailEmailAddress(email);
   }
