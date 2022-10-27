eeaf6e13e0c1a0a9be12ebb668084d1756c3dee5
Hyunggeun447 <bopi447@naver.com>
2022-10-27 15:19:19 +0900

    feat: UpdateRequest

---
+++ src/main/java/solo_project/solo_project/domain/user/dto/request/UpdateRequest.java
@@ -0,0 +1,22 @@
+package solo_project.solo_project.domain.user.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UpdateRequest {
+
+  private String nickname;
+
+  private String phoneNumber;
+
+  private String city;
+
+  private String detailAddress;
+}
