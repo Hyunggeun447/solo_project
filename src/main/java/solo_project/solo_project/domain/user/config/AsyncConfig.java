72102c61338710fd0cdd9268828896326be40d44
Hyunggeun447 <bopi447@naver.com>
2022-10-28 00:57:15 +0900

    feat: AsyncConfig

---
+++ src/main/java/solo_project/solo_project/domain/user/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package solo_project.solo_project.domain.user.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+  @Override
+  public Executor getAsyncExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(12);
+    executor.setMaxPoolSize(36);
+    executor.setQueueCapacity(24);
+    executor.setThreadNamePrefix("custom async-");
+    executor.setRejectedExecutionHandler(new CallerRunsPolicy());
+    executor.setWaitForTasksToCompleteOnShutdown(true);
+    executor.setAwaitTerminationSeconds(60);
+    executor.initialize();
+    return executor;
+  }
+}
