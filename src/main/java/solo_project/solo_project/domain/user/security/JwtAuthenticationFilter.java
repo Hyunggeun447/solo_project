73314342767172177e356d3d9a564602a3d1622c
Hyunggeun447 <bopi447@naver.com>
2022-11-05 23:11:06 +0900

    feat: 필터 변경

--- src/main/java/solo_project/solo_project/domain/user/security/JwtAuthenticationFilter.java
+++ src/main/java/solo_project/solo_project/domain/user/security/JwtAuthenticationFilter.java
@@ -1,8 +1,7 @@
 package solo_project.solo_project.domain.user.security;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureException;
+import static solo_project.solo_project.domain.user.util.SecurityConstants.LOGOUT_KEY_PREFIX;
+
 import java.io.IOException;
 import java.util.Objects;
 import javax.servlet.FilterChain;
@@ -12,12 +11,12 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.filter.GenericFilterBean;
-import solo_project.solo_project.domain.user.pojo.CustomUserDetails;
-import solo_project.solo_project.domain.user.service.CustomUserDetailService;
 
 @Slf4j
 @Component
@@ -25,37 +24,23 @@ import solo_project.solo_project.domain.user.service.CustomUserDetailService;
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
   private final JwtTokenProvider jwtTokenProvider;
-  private final CustomUserDetailService customUserDetailService;
+  private final RedisTemplate redisTemplate;
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
 
-    try {
-      String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+    String accessToken = jwtTokenProvider.resolveToken((HttpServletRequest) request);
 
-      if (Objects.nonNull(token)) {
-        String userEmail = jwtTokenProvider.getUserEmail(token);
+    if (Objects.nonNull(accessToken) && jwtTokenProvider.validateToken(accessToken)) {
+      String isLogout = (String) redisTemplate.opsForValue().get(LOGOUT_KEY_PREFIX + accessToken);
 
-        if (Objects.nonNull(userEmail)) {
-          CustomUserDetails userDetails = customUserDetailService.loadUserByUsername(userEmail);
-
-          validateAccessToken(token, userDetails);
-          Authentication authentication = jwtTokenProvider.getAuthentication(userDetails);
-          SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
+      if (ObjectUtils.isEmpty(isLogout)) {
+        Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
       }
-    } catch (SignatureException | MalformedJwtException e) {
-      log.info("토큰이 변조되었습니다.");
-    } catch (ExpiredJwtException e) {
-      log.info("토큰이 만료되었습니다.");
     }
     chain.doFilter(request, response);
   }
 
-  private void validateAccessToken(String token, CustomUserDetails userDetails) {
-    if (!jwtTokenProvider.validationToken(token, userDetails)) {
-      throw new RuntimeException();
-    }
-  }
 }
