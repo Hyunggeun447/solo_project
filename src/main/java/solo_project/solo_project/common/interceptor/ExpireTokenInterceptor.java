3036b83a593873b0c07da0df1c1ea5b206ca3eea
Hyunggeun447 <bopi447@naver.com>
2022-11-05 23:13:55 +0900

    feat: InterceptorConfig, ExpireTokenInterceptor

---
+++ src/main/java/solo_project/solo_project/common/interceptor/ExpireTokenInterceptor.java
@@ -0,0 +1,34 @@
+package solo_project.solo_project.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import solo_project.solo_project.common.exception.BadRequestException;
+import solo_project.solo_project.domain.user.security.JwtTokenProvider;
+
+@Component
+@RequiredArgsConstructor
+public class ExpireTokenInterceptor implements HandlerInterceptor {
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    String token = jwtTokenProvider.resolveToken(request);
+    if (!(authentication instanceof AnonymousAuthenticationToken)
+        && jwtTokenProvider.isTokenExpired(token)) {
+
+      throw new BadRequestException("유효하지 않은 토큰입니다.");
+    }
+
+    return HandlerInterceptor.super.preHandle(request, response, handler);
+  }
+
+}
---
+++ src/main/java/solo_project/solo_project/domain/user/config/InterceptorConfig.java
@@ -0,0 +1,30 @@
+package solo_project.solo_project.domain.user.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import solo_project.solo_project.common.interceptor.ExpireTokenInterceptor;
+
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfig implements WebMvcConfigurer {
+
+  private final ExpireTokenInterceptor expireTokenArgumentResolver;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(expireTokenArgumentResolver)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/auth/reissue");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("*");
+  }
+
+}
