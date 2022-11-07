041a6706865d2c8cd327153f9ec719e3699344f3
Hyunggeun447 <bopi447@naver.com>
2022-11-07 15:48:18 +0900

    feat: modify board api

--- src/main/java/solo_project/solo_project/domain/board/controller/BoardController.java
+++ src/main/java/solo_project/solo_project/domain/board/controller/BoardController.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import solo_project.solo_project.common.annotation.AuthUserId;
 import solo_project.solo_project.domain.board.mapper.request.CreateBoardRequest;
+import solo_project.solo_project.domain.board.mapper.request.ModifyBoardRequest;
 import solo_project.solo_project.domain.board.service.BoardService;
 
 @RequestMapping("/api/v1/board")
@@ -20,12 +23,26 @@ public class BoardController {
 
   @PostMapping()
   @ResponseStatus(HttpStatus.CREATED)
-  @PreAuthorize("hasRole('ROLE_USER')")
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
   public void createBoard(
       @AuthUserId Long userId,
-      CreateBoardRequest createBoardRequest) {
+      @RequestBody CreateBoardRequest createBoardRequest) {
 
     boardService.createBoard(userId, createBoardRequest);
   }
+
+  @PutMapping
+  @ResponseStatus(HttpStatus.OK)
+//  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN') and @checker.isLoginUsersBoard(#userId, #modifyBoardRequest.boardId)")
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+  public void modifyBoard(
+      @AuthUserId Long userId,
+      @RequestBody ModifyBoardRequest modifyBoardRequest
+  ) {
+//    boardService.modifyBoard(modifyBoardRequest);
+    boardService.modifyBoard(userId, modifyBoardRequest);
+  }
+
+
 
 }
--- src/main/java/solo_project/solo_project/domain/board/service/BoardService.java
+++ src/main/java/solo_project/solo_project/domain/board/service/BoardService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import solo_project.solo_project.domain.board.entity.Board;
 import solo_project.solo_project.domain.board.mapper.CreateBoardRequestMapper;
+import solo_project.solo_project.domain.board.mapper.ModifyBoardRequestMapper;
 import solo_project.solo_project.domain.board.mapper.request.CreateBoardRequest;
+import solo_project.solo_project.domain.board.mapper.request.ModifyBoardRequest;
 import solo_project.solo_project.domain.board.repository.BoardRepository;
 
 @Service
@@ -16,12 +18,22 @@ public class BoardService {
   private final BoardRepository boardRepository;
 
   private final CreateBoardRequestMapper createBoardRequestMapper;
+  private final ModifyBoardRequestMapper modifyBoardRequestMapper;
 
   public Long createBoard(Long userId, CreateBoardRequest createBoardRequest) {
 
     Board board = createBoardRequestMapper.toEntity(createBoardRequest);
     board.setUserId(userId);
     return boardRepository.save(board).getId();
+  }
+
+  public void modifyBoard(Long userId, ModifyBoardRequest modifyBoardRequest) {
+    Board board = boardRepository.findById(modifyBoardRequest.getBoardId())
+        .orElseThrow(RuntimeException::new);
+    if (!board.getUserId().equals(userId)) {
+      throw new RuntimeException();
+    }
+    modifyBoardRequestMapper.updateFromDto(modifyBoardRequest, board);
   }
 
 }
