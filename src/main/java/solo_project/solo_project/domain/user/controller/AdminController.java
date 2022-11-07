dbf49de42aa981d6389a9ef5fe1d744af82d4506
Hyunggeun447 <bopi447@naver.com>
2022-11-07 18:39:34 +0900

    feat: endpoint 수정

--- src/main/java/solo_project/solo_project/domain/user/controller/AdminController.java
+++ src/main/java/solo_project/solo_project/domain/user/controller/AdminController.java
@@ -3,6 +3,7 @@ package solo_project.solo_project.domain.user.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +19,14 @@ public class AdminController {
 
   private final UserService userService;
 
-  @PostMapping("delete")
+  @PostMapping("/delete")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   @Secured({"ROLE_ADMIN"})
   public void deleteUser(@RequestParam Long userId) {
     userService.delete(userId);
   }
 
+  @GetMapping("")
   @Secured({"ROLE_ADMIN"})
   public UserSelfInfoResponse getUserInfo(@RequestParam Long userId) {
     return userService.findUserSelfInfo(userId);
