9b5d8bdbabcad2d030379fb5e0e85e1b4fab2f1f
Hyunggeun447 <bopi447@naver.com>
2022-11-08 17:25:24 +0900

    feat: aop로 logTrace 적용

---
+++ src/main/java/solo_project/solo_project/common/logTrace/aop/LogTraceAspect.java
@@ -0,0 +1,39 @@
+package solo_project.solo_project.common.logTrace.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import solo_project.solo_project.common.logTrace.LogTrace;
+import solo_project.solo_project.common.logTrace.TraceStatus;
+
+@Slf4j
+@Component
+@Aspect
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Around("execution(* solo_project.solo_project.domain..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            //로직 호출
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
