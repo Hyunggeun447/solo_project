cd8e17752c9e219022c2c66a621dd749c3ff7c62
Hyunggeun447 <bopi447@naver.com>
2022-11-11 22:19:24 +0900

    feat: 어드민 공지 작성

--- src/main/java/solo_project/solo_project/domain/board/controller/AdminBoardController.java
+++ src/main/java/solo_project/solo_project/domain/board/controller/AdminBoardController.java
@@ -4,10 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import solo_project.solo_project.common.annotation.AuthUserId;
+import solo_project.solo_project.domain.board.mapper.request.CreateBoardRequest;
 import solo_project.solo_project.domain.board.service.AdminBoardService;
 
 @RequestMapping("/api/v2/board")
@@ -16,6 +20,17 @@ import solo_project.solo_project.domain.board.service.AdminBoardService;
 public class AdminBoardController {
 
   private final AdminBoardService adminBoardService;
+
+
+  @PostMapping("/notice")
+  @ResponseStatus(HttpStatus.CREATED)
+  @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+  public Long createNotice(
+      @AuthUserId Long userId,
+      @RequestBody CreateBoardRequest createBoardRequest
+  ) {
+    return adminBoardService.createNotice(userId, createBoardRequest);
+  }
 
   @DeleteMapping()
   @ResponseStatus(HttpStatus.NO_CONTENT)
--- src/main/java/solo_project/solo_project/domain/board/service/AdminBoardService.java
+++ src/main/java/solo_project/solo_project/domain/board/service/AdminBoardService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import solo_project.solo_project.domain.board.entity.Board;
+import solo_project.solo_project.domain.board.mapper.CreateBoardRequestMapper;
+import solo_project.solo_project.domain.board.mapper.request.CreateBoardRequest;
 import solo_project.solo_project.domain.board.repository.BoardRepository;
 
 @Service
@@ -12,6 +14,15 @@ import solo_project.solo_project.domain.board.repository.BoardRepository;
 public class AdminBoardService {
 
   private final BoardRepository boardRepository;
+
+  private final CreateBoardRequestMapper createBoardRequestMapper;
+
+  public Long createNotice(Long userId, CreateBoardRequest createBoardRequest) {
+    Board board = createBoardRequestMapper.toEntity(createBoardRequest);
+    boardRepository.save(board);
+    board.setUserId(userId);
+    return board.getId();
+  }
 
   public void deleteBoard(Long boardId) {
     Board board = boardRepository.findById(boardId)
