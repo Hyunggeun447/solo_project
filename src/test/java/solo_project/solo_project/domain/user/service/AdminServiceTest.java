e707539d4b063aecb132d882a37ae5d1baadf18b
Hyunggeun447 <bopi447@naver.com>
2022-11-13 12:17:43 +0900

    test: admin 권한이 있는 유저는 임의의 유저를 정지시킬 수 있습니다.

--- src/test/java/solo_project/solo_project/domain/user/service/AdminServiceTest.java
+++ src/test/java/solo_project/solo_project/domain/user/service/AdminServiceTest.java
@@ -2,6 +2,7 @@ package solo_project.solo_project.domain.user.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.UUID;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,8 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import solo_project.solo_project.domain.user.entity.Authority;
 import solo_project.solo_project.domain.user.entity.User;
 import solo_project.solo_project.domain.user.mapper.dto.request.SignUpRequest;
+import solo_project.solo_project.domain.user.pojo.CustomUserDetails;
 import solo_project.solo_project.domain.user.repository.UserRepository;
 
 @SpringBootTest
@@ -27,6 +30,9 @@ class AdminServiceTest {
   @Autowired
   UserService userService;
 
+  @Autowired
+  CustomUserDetailsService customUserDetailsService;
+
   @AfterEach
   void clean() {
     userRepository.deleteAll();
@@ -40,6 +46,8 @@ class AdminServiceTest {
   String city = "Seoul";
   String detailAddress = "street 1";
   String password = "!q2w3e4r5t";
+
+  User adminUser;
 
   @Nested
   @DisplayName("banUser test")
@@ -60,14 +68,33 @@ class AdminServiceTest {
           .detailAddress(detailAddress)
           .password(password)
           .build());
+
+      Long adminUserId = userService.signUp(SignUpRequest.builder()
+          .email("admin@google.com")
+          .firstName("관")
+          .lastName("리자")
+          .nickname("관리자")
+          .phoneNumber(phoneNumber)
+          .city(city + UUID.randomUUID().toString().substring(0, 4))
+          .detailAddress(detailAddress + UUID.randomUUID().toString().substring(0, 4))
+          .password(password + UUID.randomUUID().toString().substring(0, 4))
+          .build());
+
+      adminUser = userRepository.findById(adminUserId)
+          .orElseThrow(RuntimeException::new);
+      Authority.addAdminAuth(adminUser);
     }
 
     @Test
     @DisplayName("성공: 관리자는 임의의 계정을 lock시킬 수 있음")
     public void banUserTest() throws Exception {
 
+      //given
+      CustomUserDetails adminUserDetails = customUserDetailsService.loadUserByUsername(
+          adminUser.getEmail().getEmailAddress());
+
       //when
-      adminService.banUser(userId);
+      adminService.banUser(userId, adminUserDetails);
 
       //then
       User user = userRepository.findById(userId)
