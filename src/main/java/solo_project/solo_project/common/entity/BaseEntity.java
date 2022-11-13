b084a0a3f7d4d0c5ddeff0be66bd743b50ddd7cc
Hyunggeun447 <bopi447@naver.com>
2022-11-13 23:37:20 +0900

    feat: @EntityListeners(AuditingEntityListener.class) 삭제 및 modifiedBy 변경 가능, 컬럼명 변경

--- src/main/java/solo_project/solo_project/common/entity/BaseEntity.java
+++ src/main/java/solo_project/solo_project/common/entity/BaseEntity.java
@@ -6,19 +6,17 @@ import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity extends BaseTimeEntity{
+public abstract class BaseEntity extends BaseTimeEntity {
 
   @CreatedBy
   @Column(name = "created_by", updatable = false)
   private String createdBy;
 
   @LastModifiedBy
-  @Column(name = "modifiedBy", updatable = false)
+  @Column(name = "modified_by")
   private String modifiedBy;
 
 }
