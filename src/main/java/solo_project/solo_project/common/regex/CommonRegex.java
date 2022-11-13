56987c95240459c2f2d882971a1da5d2ca3e3ef6
Hyunggeun447 <bopi447@naver.com>
2022-11-13 10:41:19 +0900

    fix: 전화번호 regex 변경 -> 만약 - 이 없어도 패턴이 일치

--- src/main/java/solo_project/solo_project/common/regex/CommonRegex.java
+++ src/main/java/solo_project/solo_project/common/regex/CommonRegex.java
@@ -8,6 +8,6 @@ public class CommonRegex {
 
   public static final String PASSWORD_REGEX_PATTERN = "^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\\-_=+]).{8,15}$";
 
-  public static String PHONE_NUMBER_REGEX_PATTERN = "^\\d{3}-\\d{4}-\\d{4}$";
+  public static String PHONE_NUMBER_REGEX_PATTERN = "^\\d{3}-?\\d{4}-?\\d{4}$";
 
 }
