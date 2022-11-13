33b9e39bb0f79786d7a55412f04dd443c2d13a07
Hyunggeun447 <bopi447@naver.com>
2022-11-13 16:44:15 +0900

    clear

--- src/main/java/solo_project/solo_project/domain/user/repository/AuthorityRepository.java
+++ src/main/java/solo_project/solo_project/domain/user/repository/AuthorityRepository.java
@@ -1,16 +1,12 @@
 package solo_project.solo_project.domain.user.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import solo_project.solo_project.domain.user.entity.Authority;
-import solo_project.solo_project.domain.user.entity.User;
 
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-
-  Optional<Authority> findByUserAndRole(User user, String role);
 
   @Modifying(clearAutomatically = true)
   @Query("update Authority a set a.isDeleted = true where a.user.id =:userId and a.role =:role")
