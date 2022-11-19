9f7cf8478e134b967cad1b699a5641c92add499b
Hyunggeun447 <bopi447@naver.com>
2022-11-19 20:29:37 +0900

    test : clean

--- src/test/java/solo_project/solo_project/domain/board/service/BoardServiceTest.java
+++ src/test/java/solo_project/solo_project/domain/board/service/BoardServiceTest.java
@@ -3,7 +3,6 @@ package solo_project.solo_project.domain.board.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
