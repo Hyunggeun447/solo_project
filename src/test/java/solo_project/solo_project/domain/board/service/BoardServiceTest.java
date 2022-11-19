2d599a4895f3d7bfaf6963d7311b4ced4e7c6dc6
Hyunggeun447 <bopi447@naver.com>
2022-11-19 20:03:31 +0900

    test: board 수정 Test

--- src/test/java/solo_project/solo_project/domain/board/service/BoardServiceTest.java
+++ src/test/java/solo_project/solo_project/domain/board/service/BoardServiceTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import solo_project.solo_project.domain.board.entity.Board;
 import solo_project.solo_project.domain.board.mapper.request.CreateBoardRequest;
+import solo_project.solo_project.domain.board.mapper.request.ModifyBoardRequest;
 import solo_project.solo_project.domain.board.repository.BoardRepository;
 import solo_project.solo_project.domain.board.value.BoardType;
 
@@ -29,6 +32,11 @@ class BoardServiceTest {
   String DESCRIPTION = "I can write description";
   Long USER_ID = 1L;
   BoardType BOARD_TYPE = BoardType.NORMAL;
+
+  @AfterEach
+  void clear() {
+    boardRepository.deleteAll();
+  }
 
   @Nested
   @DisplayName("createBoard test")
@@ -59,6 +67,53 @@ class BoardServiceTest {
       assertThat(USER_ID).isEqualTo(board.getUserId());
     }
 
+  }
+
+  @Nested
+  @DisplayName("modifyBoard test")
+  class ModifyBoardTest {
+
+    Long boardId;
+
+    String NEW_TITLE = "수정할 제목";
+    String NEW_DESCRIPTION = "수정할 내용이이이이이이";
+
+    @BeforeEach
+    void setup() {
+
+      //given
+      CreateBoardRequest createBoardRequest = CreateBoardRequest.builder()
+          .title(TITLE)
+          .description(DESCRIPTION)
+          .boardType(BOARD_TYPE)
+          .build();
+
+      //when
+      boardId = boardService.createBoard(USER_ID, createBoardRequest);
+    }
+
+    @Test
+    @DisplayName("성공: board 수정 성공")
+    public void modifyBoardTest() throws Exception {
+
+      //given
+      ModifyBoardRequest modifyBoardRequest = ModifyBoardRequest.builder()
+          .boardId(boardId)
+          .title(NEW_TITLE)
+          .description(NEW_DESCRIPTION)
+          .build();
+
+      //when
+      boardService.modifyBoard(USER_ID, modifyBoardRequest);
+
+      //then
+      Board board = boardRepository.findById(boardId).orElseThrow(RuntimeException::new);
+
+      assertThat(modifyBoardRequest)
+          .usingRecursiveComparison()
+          .ignoringFields("boardId")
+          .isEqualTo(board);
+    }
   }
 
 }
